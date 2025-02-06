eaeb1d11a2446eda002f7f90272b7660047760b0
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2025-02-06 11:16:18 +0300

    creating a db in sqlalchemy

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,3 +1,5 @@
+# lib/sqlalchemy_sandbox.py
+
 #!/usr/bin/env python3
 
 from sqlalchemy import Column, Integer, String, create_engine
@@ -5,8 +7,17 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
+
 class Student(Base):
-    pass
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key = True)
+    name =  Column(String())
+
+
+
 
 if __name__ == '__main__':
-    pass
+    engine = create_engine('sqlite:///students.db')
+    Base.metadata.create_all(engine)
